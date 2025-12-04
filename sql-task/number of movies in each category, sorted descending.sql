58a361291f1bfaafbf9da7a7efddb5573140d432
BUNYOD YOKUBOV <117659225+Bunyod-Y@users.noreply.github.com>
2025-12-04 18:10:35 +0500

    Add SQL query to count movies by category

---
+++ sql-task/number of movies in each category, sorted descending.sql
@@ -0,0 +1,7 @@
+SELECT c.name AS category,
+       COUNT(f.film_id) AS film_count
+FROM category c
+JOIN film_category fc ON c.category_id = fc.category_id
+JOIN film f ON fc.film_id = f.film_id
+GROUP BY c.category_id, c.name
+ORDER BY film_count DESC;
